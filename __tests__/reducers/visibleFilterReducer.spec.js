51076a0f546a5ecb380e4c05f93e1380a66f51d3
sho.tanaka <sho.tanaka@aijus.com>
2019-12-13 17:34:29 +0900

    fixed for typo

--- __tests__/reducers/visibleFilterReducer.spec.js
+++ __tests__/reducers/visibleFilterReducer.spec.js
@@ -9,13 +9,13 @@ import {
 } from '../../actions/visibleFilterActionCreator';
 
 describe('reducer/visibleFilterReducer test', () => {
-    const createInithialState = () => {
+    const createInitialState = () => {
         const mockAction = {};
         const state = visibleFilterReducer(undefined, mockAction);
         return state;
     };
     it("The initial value of state is SHOW_ALL", () => {
-        const state = createInithialState();
+        const state = createInitialState();
         expect(state).toStrictEqual(SHOW_ALL);
     });
     it("return string 'SHOW_ALL' when action.type === SHOW_ALL", () => {
@@ -26,14 +26,14 @@ describe('reducer/visibleFilterReducer test', () => {
     });
     it("return SHOW_ACTIVE when action.type === SHOW_ACTIVE", () => {
 
-        const state = createInithialState();
+        const state = createInitialState();
         const action = showActive();
         const newState = visibleFilterReducer(state, action);
         expect(newState).toStrictEqual(SHOW_ACTIVE);
     });
     it("return SHOW_COMPLETED when action.type === SHOW_COMPLETED", () => {
 
-        const state = createInithialState();
+        const state = createInitialState();
         const action = showCompleted();
         const newState = visibleFilterReducer(state, action);
         expect(newState).toStrictEqual(SHOW_COMPLETED);
